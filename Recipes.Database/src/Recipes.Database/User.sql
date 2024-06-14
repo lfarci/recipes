c343802cdddd48f4166881f800c19f8c146f9944
Logan Farci <farci.logan@gmail.com>
2024-06-14 22:27:27 +0200

    Add project files.

---
+++ Recipes.Database/src/Recipes.Database/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [Id] BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [FirstName] NVARCHAR(100) NOT NULL,
+    [LastName] NVARCHAR(100) NOT NULL,
+
+    CONSTRAINT [CK_User_FirstName_NotEmpty] CHECK ([FirstName] <> ''),
+    CONSTRAINT [CK_User_LastName_NotEmpty] CHECK ([LastName] <> '')
+)
