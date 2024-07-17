e463f85d0266ad885088cd20bdcd8317e773e1df
Logan Farci <farci.logan@gmail.com>
2024-07-17 08:06:58 +0200

    Remove unused using statement in the recipe endpoints extensions class

--- src/Recipes.Api/Recipes/RecipeEndpoints.cs
+++ src/Recipes.Api/Recipes/RecipeEndpoints.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web.Resource;
 using System.Security.Claims;
 
